b8c372921fecdeeb14e1a7f5ba338787b17e37ec
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 18:27:08 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,16 @@
 class Apppointment
-  attr_reader :date, :patient, :doctor 
+  attr_reader :date, :patient, :doctor
   @@all = []
   def initialize (date, patient, doctor)
-    @date = date 
-    @patient = patient 
-    @doctor = doctor 
-    @@all << self 
-  end 
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
-  
+
 end
