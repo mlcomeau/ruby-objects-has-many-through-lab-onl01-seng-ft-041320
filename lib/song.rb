34a06af1124dc835dbeb41ee51cccabccd443265
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 18:15:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,9 @@
 class Song
-  attr_reader :name, :genre
+  attr_reader :name, :artist, :genre
   @@all = []
-  def initialize (name, genre)
+  def initialize (name, artist, genre)
     @name = name
+    @artist = artist 
     @genre = genre
     @@all << self
   end
