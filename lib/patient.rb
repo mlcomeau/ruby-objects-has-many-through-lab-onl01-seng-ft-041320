7ff10210aab75b2ececf449c2914b87c5e076b2f
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 18:37:41 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,7 +15,7 @@ class Patient
   end
 
   def appointments
-    Apppointment.all.select {|appt| appt.patient == self}
+    Appointment.all.select {|appt| appt.patient == self}
   end
 
   def doctors
