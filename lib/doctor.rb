4ff479b79de4689b0bb84093e82773fd37fea907
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 18:20:49 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,2 +1,30 @@
 class Doctor
+  attr_reader :name 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def new_appointment(date, patient)
+    new_appointment = Apoointment.new(date, patient, self)
+  end 
+
+  def appointments 
+    Apppointment.all.select {|appt| appt.doctor == self}
+  end 
+
+  def patients 
+    Apppointment.all.select do |appt|
+      if appt.doctor == self 
+        appt.patient
+      end
+    end
+  end
+  
+    
 end
